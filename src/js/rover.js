193dfb6623a7cfaed86c83525caf46487082c7fc
Tagg McDonald <taggmcd@gmail.com>
2024-02-19 18:49:20 -0700

    Add localstorage for rover form

--- src/js/rover.js
+++ src/js/rover.js
@@ -1,29 +1,33 @@
 import RoverControls from "./roverControls.mjs";
 import Rover from "./rover.mjs";
 import ImageList from "./images.mjs";
-import { getUrlParam } from "./utils";
+import { getUrlParam, setLocalStorage, getLocalStorage } from "./utils";
 
 addEventListener("load", () => {
-  let rover = getUrlParam("rover");
-  let camera = getUrlParam("camera");
-  let earth_date = getUrlParam("earth_date");
+  const form = document.getElementById("rover-select");
+  const roverSelect = document.getElementById("rover");
+  const cameraSelect = document.getElementById("camera");
+  const earthDateSelect = document.getElementById("earth_date");
+
+  let rover = getLocalStorage("rover");
+  let camera = getLocalStorage("camera");
+  let earth_date = getLocalStorage("earth_date");
 
   new RoverControls(rover, camera, earth_date).init();
-
   new Rover(rover).init();
 
-  const form = document.getElementById("rover-select");
-
+  roverSelect.addEventListener("change", (event) => {
+    new Rover(event.target.value).init();
+  });
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    const roverSelect = document.getElementById("rover");
-    const cameraSelect = document.getElementById("camera");
-    const earthDateSelect = document.getElementById("earth_date");
 
     rover = roverSelect.value;
     camera = cameraSelect.value;
     earth_date = earthDateSelect.value;
+    setLocalStorage("rover", rover, 9000);
+    setLocalStorage("camera", camera, 9000);
+    setLocalStorage("earth_date", earth_date, 9000);
 
     new Rover(rover).init();
     new ImageList(rover, camera, earth_date).init();
